93d5db1d324891a657cab1d91e483292000d9492
Giordano Dìaz <diazgiordano@gmail.com>
2020-08-19 12:16:44 -0500

    My_each method finished

--- enumerables.rb
+++ enumerables.rb
@@ -1,8 +1,13 @@
 module Enumerable
   def my_each
-  # your code here
-  
+    return to_enum(:my_each) unless block_given?
+    n = Array(self).length
+    i = 0
+    # You can use the times method insted while
+    while i < n
+      yield(Array(self)[i])
+      i+=1
+    end
+    self
   end
 end
-
-my_each() { |i| }
