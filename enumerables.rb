1916da234aa998bc66d7ce369888fcf8e341a6ad
Giordano Dìaz <diazgiordano@gmail.com>
2020-08-19 12:26:43 -0500

    my_each_with_index method finished

--- enumerables.rb
+++ enumerables.rb
@@ -10,4 +10,16 @@ module Enumerable
     end
     self
   end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+    n = Array(self).length
+    i = 0
+    # You can use the times method insted while
+    while i < n
+      yield(Array(self)[i], i)
+      i+=1
+    end
+    self
+  end
 end
